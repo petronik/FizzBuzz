36cf7a8025152144b69c493da28272ba261f582c
Petronik <petronik@ukr.net>
2021-07-03 15:10:42 -0500

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using static System.Console;
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            //try
+            //{
+
+            //    WriteLine("Enter a number between 0 and 255:");
+
+            //    a = Convert.ToInt32(ReadLine());
+            //    if (a < 0 || a > 255)
+            //    {
+            //        throw new ArgumentOutOfRangeException();
+            //    }
+            //    WriteLine("Enter another number between 0 and 255:");
+            //    b = Convert.ToInt32(ReadLine());
+            //    if (b < 0 || b > 255)
+            //    {
+            //        throw new ArgumentOutOfRangeException();
+            //    }
+            //    WriteLine($"{a} divided by {b} is {a/b}");
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+            //catch (FormatException e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+            //catch (Exception e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+
+
+            try
+            {
+
+                WriteLine("Enter a number between 0 and 255:");
+                a = Convert.ToInt32(ReadLine());
+                WriteLine("Enter another number between 0 and 255:");
+                b = Convert.ToInt32(ReadLine());
+                if ((a < 0 || a > 255) || (b < 0 || b > 255))
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                WriteLine($"{a} divided by {b} is {a / b}");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                WriteLine("ArgumentOutOfRangeException: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                WriteLine("FormatException: " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                WriteLine("DivideByZeroException: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                WriteLine("Exception: " + e.Message);
+            }
+
+
+            WriteLine("Have a nice day!!!");
+        }
+    }
+}
